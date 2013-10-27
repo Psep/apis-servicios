8c602c6431e5854abf1e83829371896cb4514e01
Pablo Sepulveda <psep@debian>
2013-10-27 11:58:43 -0300

    Add documentacion

--- Bip.php
+++ Bip.php
@@ -1,13 +1,42 @@
 <?php
-
-require_once 'GenericUtils.php';
+/**
+ * Copyright (C) 2013 Pablo Sepúlveda P. <psep.pentauc@gmail.com>
+ *
+ * This file is part of apis-servicios.
+ * apis-servicios is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * apis-servicios is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with apis-servicios.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
- * @author psep
- * @license GPL v3
+ * Include GenericUtils class
+ */  
+require_once 'GenericUtils.php'; 
+ 
+/**
+ * This class cointains utils read from Consulta Tarjeta Bip!.
+ * @author Pablo Sepúlveda P. <psep.pentauc@gmail.com>
+ * @version 1.0
+ * @package apis-servicios
+ * @copyright Copyright (C) 2010-2013 Pablo Sepúlveda P.
+ * @license GNU GPLv3 or later
+ * @link http://www.psep.cl
  */
 class Bip {
 
+	/**
+	 * Atributo privado donde recaerá el valor del
+	 * ID de la tarjeta BIP!.
+	 */
 	private $idNumber;
 
 	/**
@@ -72,7 +101,19 @@ class Bip {
 
 }
 
+/**
+ * Instancia de la clase Bip incluyendo
+ * método GET como valor:
+ * 
+ * Bip.php?numberBip=xxxxxxx
+ * 
+ * Donde xxxxxxx es el número de la tarjeta.
+ */
 $bip = new Bip(htmlspecialchars($_GET["numberBip"]));
+
+/**
+ * Ejecución de la función getData.
+ */
 print_r($bip -> getData());
 
 ?>
